81472e74c9d86755eaee93d98dee03080a49d8f5
yanshuai <yanshuai@jovision.com>
2014-10-17 15:39:58 +0800

    morefragment modify

--- src/com/jovision/adapters/FragmentAdapter.java
+++ src/com/jovision/adapters/FragmentAdapter.java
@@ -9,11 +9,11 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.jovetech.CloudSee.temp.R;
 import com.jovision.bean.MoreFragmentBean;
+import com.jovision.commons.MySharedPreference;
 
 public class FragmentAdapter extends BaseAdapter {
 	private Context activity;
@@ -56,22 +56,49 @@ public class FragmentAdapter extends BaseAdapter {
 					.findViewById(R.id.item_img);
 			holder.item_new = (ImageView) convertView
 					.findViewById(R.id.item_new);
+			holder.item_next = (ImageView) convertView
+					.findViewById(R.id.item_next);
 			holder.name = (TextView) convertView.findViewById(R.id.item_name);
-			holder.item_linear = (LinearLayout) convertView
-					.findViewById(R.id.item_linear);
 			convertView.setTag(holder);
 		} else {
 			holder = (ViewHolder) convertView.getTag();
 		}
 		holder.item_img.setBackgroundResource(dataList.get(position)
 				.getItem_img());
-		holder.item_linear.setBackgroundResource(dataList.get(position)
-				.getItem_linear());
 		holder.name.setText(dataList.get(position).getName());
+		if (position > 3 && position < 7) {
+			holder.item_next
+					.setBackgroundResource(R.drawable.morefragment_normal_icon);
+			switch (position) {
+			case 4:
+				if (MySharedPreference.getBoolean("HELP")) {
+					holder.item_next
+							.setBackgroundResource(R.drawable.morefragment_selector_icon);
+				}
+				break;
+			case 5:
+				if (MySharedPreference.getBoolean("AlarmSwitch")) {
+					holder.item_next
+							.setBackgroundResource(R.drawable.morefragment_selector_icon);
+				}
+				break;
+			case 6:
+				if (MySharedPreference.getBoolean("PlayDeviceMode")) {
+					holder.item_next
+							.setBackgroundResource(R.drawable.morefragment_selector_icon);
+				}
+				break;
+			default:
+				break;
+			}
+		}
 		if (dataList.get(position).isIsnew()) {
-			if (position == 2) {
+			if (position == 3) {
 				holder.item_new
 						.setBackgroundResource(R.drawable.morefragment_red_icon);
+			} else if (position == 1) {
+				holder.item_new
+						.setBackgroundResource(R.drawable.morefragment_yellow_icon);
 			}
 			holder.item_new.setVisibility(View.VISIBLE);
 		} else {
@@ -84,6 +111,7 @@ public class FragmentAdapter extends BaseAdapter {
 		private ImageView item_img;
 		private TextView name;
 		private ImageView item_new;
-		private LinearLayout item_linear;
+		private ImageView item_next;
+
 	}
 }

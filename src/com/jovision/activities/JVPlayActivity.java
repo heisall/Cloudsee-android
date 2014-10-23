2549155218c26e4dab73040557aafc9a6baf4fff
yanshuai <yanshuai@jovision.com>
2014-10-23 18:49:50 +0800

    modify

--- src/com/jovision/activities/JVPlayActivity.java
+++ src/com/jovision/activities/JVPlayActivity.java
@@ -100,6 +100,7 @@ public class JVPlayActivity extends PlayActivity implements
 
 		switch (what) {
 		case Consts.CALL_CONNECT_CHANGE: {
+			Log.i("TAG", channelList.size()+"size"+arg1);
 			Channel channel = channelList.get(arg1);
 			if (null == channel) {
 				return;

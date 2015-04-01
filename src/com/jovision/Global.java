430cd856c9e4f912cfe442076767266696151b6d
liukp <liukunpeng@jovision.com>
2015-04-01 10:41:43 +0800

    添加流量统计接口

---
+++ src/com/jovision/Global.java
@@ -0,0 +1,8 @@
+package com.jovision;
+
+public class Global {
+	/*云存储报警视频流量统计*/
+	public static boolean TRAFFICSTATS_SUPPORTED = true; //是否支持流量统计接口标志
+	public static boolean USE_VITAMIO = false; //是否强制使用Vitamio播放在线视频
+	public static String STR_TRAFFICSTATS_CMPT_RES = "";
+}

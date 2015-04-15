f99712e6392be7776478b883630eff34aa3631d6
nihy <nihongyun@jovision.com>
2015-04-15 16:57:22 +0800

    修改代码错误

--- src/com/jovision/MainApplication.java
+++ src/com/jovision/MainApplication.java
@@ -678,7 +678,7 @@ public class MainApplication extends Application implements IHandlerLikeNotify {
             }
             if (Boolean.valueOf(statusHashMap.get(Consts.LOCAL_LOGIN))
                     && (itemFlag.equals(Consts.MORE_REMEMBER) ||
-                       itemFlag.equals(Consts.MORE_CLOUD_SHOP))) {
+                    itemFlag.equals(Consts.MORE_CLOUD_SHOP))) {
                 item.setDismiss(true);
             }
             if (itemFlag.equals(Consts.MORE_HELP)
